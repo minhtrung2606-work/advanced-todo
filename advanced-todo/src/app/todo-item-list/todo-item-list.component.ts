9b77ef0f647c05f265164652a6d65c82186cf7a0
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-16 22:08:41 +0700

    Move template to TodoItemList component. Update TodoItemList to interact with TodoItem component

--- advanced-todo/src/app/todo-item-list/todo-item-list.component.ts
+++ advanced-todo/src/app/todo-item-list/todo-item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './../todo-item/todo-item';
 
 @Component({
   selector: 'todo-item-list',
@@ -8,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class TodoItemListComponent implements OnInit {
   abstract;
 
-  constructor() { }
+  public todoItemList:Array<TodoItem>;
+
+  constructor() {
+    this.todoItemList = [];
+  }
 
   ngOnInit() {
+    if (this.todoItemList.length === 0) {
+      let todoItem = new TodoItem('TODO #1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum');
+      this.todoItemList.push(todoItem);
+    }
+  }
+
+  onTickTodoItem(todoItem: TodoItem): void {
+    todoItem.toggleDone();
   }
 
 }
