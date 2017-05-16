203197df574d66a818f18c75f82be0758b918c63
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-16 20:30:14 +0700

    Add TodoItem model

---
+++ advanced-todo/src/app/todo-item/todo-item.ts
@@ -0,0 +1,49 @@
+export class TodoItem {
+  private done:Boolean;
+  private title:String;
+  private desc:String;
+  private creationDate:Date;
+  private updateDate:Date;
+  private doneDate:Date;
+
+  constructor(title:String, desc:String) {
+    this.done = false;
+    this.title = title || '';
+    this.desc = desc || '';
+  }
+
+  done(): TodoItem {
+    this.done = true;
+    return this;
+  }
+
+  undone():TodoItem {
+    this.done = false;
+    return this;
+  }
+
+  setTitle(title:String):TodoItem {
+    this.title = title;
+    return this;
+  }
+
+  setDescription(desc:String):TodoItem {
+    this.desc = desc;
+    return this;
+  }
+
+  setCreationDate(creationDate:Date):TodoItem {
+    this.creationDate = creationDate;
+    return this;
+  }
+
+  setUpdateDate(updateDate:Date):TodoItem {
+    this.updateDate = updateDate;
+    return this;
+  }
+
+  setDoneDate(doneDate:Date):TodoItem {
+    this.doneDate = doneDate;
+    return this;
+  }
+}
