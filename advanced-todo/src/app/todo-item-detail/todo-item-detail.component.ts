89ba221f1418cdd043b9c29af3038fe1654c893b
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-17 01:18:09 +0700

    Update TodoItemDetail to have input

--- advanced-todo/src/app/todo-item-detail/todo-item-detail.component.ts
+++ advanced-todo/src/app/todo-item-detail/todo-item-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { TodoItem } from './../todo-item/todo-item';
 
 @Component({
@@ -9,14 +9,10 @@ import { TodoItem } from './../todo-item/todo-item';
 export class TodoItemDetailComponent implements OnInit {
   abstract;
 
+  @Input()
   public todoItem:TodoItem;
 
-  constructor() {
-    //this.todoItem = new TodoItem('TODO #1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum');
-    //this.todoItem
-    //  .markDone()
-    //;
-  }
+  constructor() { }
 
   ngOnInit() {
   }
